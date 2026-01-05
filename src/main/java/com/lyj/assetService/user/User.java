b9e12b6b903b52940d2b61e80be6cd5fda39e76d
lzeroj <dladudwns366@naver.com>
2026-01-05 18:26:44 +0900

    2025.01.05 초기 세팅

--- src/main/java/com/lyj/assetService/user/User.java
+++ src/main/java/com/lyj/assetService/user/User.java
@@ -1,4 +1,27 @@
 package com.lyj.assetService.user;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "userinfo")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userid;
+    @Column
+    private String password;
+    @Column
+    private String kornm;
+    @Column
+    private String engnm;
+    @Column
+    private String email;
+    @Column
+    private String ssn;
+    @Column
+    private String birthday;
+    @Column
+    private String telno;
 }
--- src/main/java/com/lyj/assetService/user/UserController.java
+++ src/main/java/com/lyj/assetService/user/UserController.java
@@ -1,4 +1,12 @@
 package com.lyj.assetService.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 }

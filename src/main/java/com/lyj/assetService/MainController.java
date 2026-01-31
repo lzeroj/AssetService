32ccb6639d0128be0166919b62de5ac84213740c
lzeroj <dladudwns366@naver.com>
2026-01-31 13:02:59 +0900

    2026.01.31 리액트 연동 테스트

--- src/main/java/com/lyj/assetService/MainController.java
+++ src/main/java/com/lyj/assetService/MainController.java
@@ -1,13 +1,26 @@
 package com.lyj.assetService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class MainController {
 
-    @GetMapping("/")
-    public String main(){
-        return "main";
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Map<String,String> user) {
+        String id = user.get("id");
+        String pw = user.get("pw");
+
+        if("test".equals(id) && "123".equals(pw)) {
+            return ResponseEntity.ok("OK"); // 로그인 성공
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Fail");
     }
 }

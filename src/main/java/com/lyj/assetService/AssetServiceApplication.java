33859e20fe5dd8f685a47dd84e63cbd81526547f
임영준 <yjlim@tomatosystem.co.kr>
2026-02-08 16:46:48 +0900

    2026.02.08 테이블 자동생성 설정

--- src/main/java/com/lyj/assetService/AssetServiceApplication.java
+++ src/main/java/com/lyj/assetService/AssetServiceApplication.java
@@ -2,8 +2,10 @@ package com.lyj.assetService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AssetServiceApplication {
 
 	public static void main(String[] args) {
